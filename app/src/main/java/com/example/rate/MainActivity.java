46f3f3625a310588c958ea444c45e83928a6ea66
mxz07 <710690219@qq.com>
2022-04-28 19:40:53 +0800

    Initial commit

---
+++ app/src/main/java/com/example/rate/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.rate;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity{
+
+    float rate = 0f;
+    String title ="";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        title=getIntent().getStringExtra("huobi");
+        rate=getIntent().getFloatExtra("huilv",0f);
+        Log.i("TAG", title+rate);
+        ((TextView)findViewById(R.id.huobi)).setText(title);
+    }
+    //处理汇率转换事件
+    public void change(View v){
+        EditText edit=(EditText) findViewById(R.id.rmb);
+        TextView res=(TextView)findViewById(R.id.xianshi);
+        Log.i("main","onClick Btn");
+        String strInputRMB=edit.getText().toString();
+        if (strInputRMB.length() == 0){
+            res.setText("请输入金额！");
+            return;
+        }
+        double inputRMB=Float.parseFloat(strInputRMB);
+        res.setText(String.format("结合人民币："+"%.2f",inputRMB*inputRMB));
+    }
+}
---
+++ app/src/main/java/com/example/rate/MyAdapter.java
@@ -0,0 +1,38 @@
+package com.example.rate;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MyAdapter extends ArrayAdapter {
+    private static final String TAG = "MyAdapter";
+
+    public MyAdapter(
+            Context context,
+            int resource,
+            ArrayList<HashMap<String, String>> list) {
+        super(context, resource, list);
+    }
+    @Override
+    public View getView(int position, View converView, ViewGroup parent){
+        View itemView = converView;
+        if(itemView == null){
+            itemView = LayoutInflater.from(getContext()).inflate(R.layout.list_item,parent,false);
+        }
+
+        Map<String,String> map = (Map<String,String>) getItem(position);
+        TextView title = (TextView) itemView.findViewById(R.id.textView);
+        TextView detail = (TextView) itemView.findViewById(R.id.textView2);
+
+        title.setText(map.get("huobi"));
+        detail.setText(map.get("huilv"));
+
+        return itemView;
+    }
+}
